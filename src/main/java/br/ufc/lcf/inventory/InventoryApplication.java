bc638cdf27eb7572930db66b35630d798f9d32bf
San <jbsanthiago@gmail.com>
2023-06-02 09:07:19 -0300

    Configuração de endereçamento IP 2

--- src/main/java/br/ufc/lcf/inventory/InventoryApplication.java
+++ src/main/java/br/ufc/lcf/inventory/InventoryApplication.java
@@ -2,8 +2,9 @@ package br.ufc.lcf.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 public class InventoryApplication {
 
 	public static void main(String[] args) {
